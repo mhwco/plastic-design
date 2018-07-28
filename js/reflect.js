dbf2fe8a32cec3908302e2333eeec630aa8db490
CrSjimo <crs_16423@outlook.com>
2018-07-28 09:31:16 +0800

    改bug

--- js/reflect.js
+++ js/reflect.js
@@ -1,10 +1,7 @@
+"use strict";
 // JavaScript Document
 $(document).ready(function(){
 	$(".left-top-dialog,.center-top-dialog,.right-top-dialog,.left-center-dialog,	.center-dialog,.right-center-dialog,.left-bottom-dialog,.center-bottom-dialog,.right-bottom-dialog").hide();
-	$("#mask").click(function(){
-		$(".left-top-dialog,.center-top-dialog,.right-top-dialog,.left-center-dialog,	.center-dialog,.right-center-dialog,.left-bottom-dialog,.center-bottom-dialog,.right-bottom-dialog").hide(200);
-		$("#mask").hide();
-	});
 });
 var rf={
 	test:function(){
@@ -14,6 +11,10 @@ var rf={
 		create:function(){
 			$("body").prepend('<div id="mask"></div>');
 			$("#mask").hide();
+			$("#mask").click(function(){
+				$(".left-top-dialog,.center-top-dialog,.right-top-dialog,.left-center-dialog,	.center-dialog,.right-center-dialog,.left-bottom-dialog,.center-bottom-dialog,.right-bottom-dialog").hide(200);
+				$("#mask").hide();
+			});
 		},
 		show:function(){
 			$("#mask").show();
@@ -24,37 +25,37 @@ var rf={
 		toogle:function(){
 			$("#mask").toogle();
 		},
-		isOpen:functuon(){
+		isOpen:function(){
 			return $("#mask").is(":visible");
 		}
 	},
 	dialog:function(s){
 		return {
 			toogle:function(with_mask){
-				if(with_mask = undefined || with_mask = null){
+				if(with_mask === undefined || with_mask === null){
 					with_mask=true;
 				}
 				$("#"+"dialog-"+s).toggle(200);
 				if(with_mask){
-					this.mask.toogle();
+					rf.mask.toogle();
 				}
 			},
 			open:function(with_mask){
-				if(with_mask = undefined || with_mask = null){
+				if(with_mask === undefined || with_mask === null){
 					with_mask=true;
 				}
 				$("#"+"dialog-"+s).show(200);
 				if(with_mask){
-					this.mask.show();
+					rf.mask.show();
 				}
 			},
 			close:function(with_mask){
-				if(with_mask = undefined || with_mask = null){
+				if(with_mask === undefined || with_mask === null){
 					with_mask=true;
 				}
 				$("#"+"dialog-"+s).hide(200);
 				if(with_mask){
-					this.mask.hide();
+					rf.mask.hide();
 				}
 			},
 			isOpen:function(){
