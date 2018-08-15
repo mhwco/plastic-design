146bfa1bbd473e0ef1d33b902729495a45a73683
CrSjimo <crs_16423@outlook.com>
2018-08-15 14:04:04 +0800

    go

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	rf.dialog("language-select").create().build({
 		type:1,
 		position:2,
-		title:"Select a language:",
+		title:'<marquee scrollamount="5px" onmouseover="this.stop()" onmouseout="this.start()">اختر لغة 选择一个语言 Select a language Sélectionnez une langue Выберите язык Selecciona un idioma</marquee>',
 		positive:"OK",
 		items:["English(en)","中文(中国)(zh-CN)"],
 		onReturn:function(method,item){
